538937c73a8826ef3a4ac67aab98b12fede31afc
SeraphDev6 <wesley.giles@gmail.com>
2021-08-02 20:40:09 -0400

    Updated to chain methods

--- user.py
+++ user.py
@@ -8,6 +8,7 @@ class User:
         self.balance+=amount
         print(f"Successfully deposited ${amount} into {self.name}'s account.")
         print(f"{self.name}'s current balance is ${self.balance}")
+        return self
 
     def make_withdrawl(self,amount):
         if self.balance >= amount:
@@ -17,9 +18,11 @@ class User:
         else:
             print(f"{self.name} does not have enough funds to make this withdrawl")
             print(f"{self.name}'s current balance is ${self.balance}")
+        return self
 
     def display_user_balance(self):
         print(f"{self.name}'s current balance is ${self.balance}")
+        return self
 
     def transfer_money(self,other_user ,amount):
         if self.balance >= amount:
@@ -31,25 +34,14 @@ class User:
         else:
             print(f"{self.name} does not have enough funds to make this transfer")
             print(f"{self.name}'s current balance is ${self.balance}")
+        return self
 
 wesley=User("Wesley",2050)
 peter=User("Peter",9001)
 marius=User("Marius",700)
 
-wesley.make_deposit(200)
-wesley.make_deposit(900)
-wesley.make_deposit(210)
-wesley.make_withdrawl(3000)
-wesley.display_user_balance()
+wesley.make_deposit(200).make_deposit(900).make_deposit(210).make_withdrawl(3000).display_user_balance()
 
-peter.make_deposit(900)
-peter.make_deposit(210)
-peter.make_withdrawl(3000)
-peter.make_withdrawl(500)
-peter.display_user_balance()
+peter.make_deposit(900).make_deposit(210).make_withdrawl(3000).make_withdrawl(500).display_user_balance()
 
-marius.make_withdrawl(3000)
-marius.make_deposit(21000)
-marius.make_withdrawl(3000)
-marius.make_withdrawl(500)
-marius.display_user_balance()
+marius.make_withdrawl(3000).make_deposit(21000).make_withdrawl(3000).make_withdrawl(500).display_user_balance()
